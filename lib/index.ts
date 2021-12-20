89c170bddbebc51d3ee8ed14404214a1e40a6d1f
Ash Wilson <smashwilson@github.com>
2021-12-20 14:41:21 -0500

    One-off query

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,42 @@
-export function placeholder(): string {
-  return "works";
+import args from "args";
+import { GraphQL } from "./graphQL";
+import { PullRequestLocator } from "./service/pullRequestLocator";
+import { inspect } from "util";
+
+export class TerminationError {
+  name: string = "TerminationError";
+  message: string = "Throw to terminate the process with an abnormal exit status";
+  exitCode: number;
+
+  constructor(exitCode: number = 1) {
+    this.exitCode = exitCode;
+  }
+}
+
+export async function main(argv: string[]): Promise<void> {
+  args
+    .option(["t", "token"], "GitHub API token used for queries", "")
+    .option(["r", "repo"], "Limit results to PRs in this repo", [])
+    .option(["w", "wait"], "Poll for updates", false);
+  const flags = args.parse(argv);
+
+  const token: string = flags.token || process.env.GH_GH_PAT || process.env.GITHUB_TOKEN;
+  if (!token) {
+    console.error("You must specify an access token via one of:")
+    console.error("- $GITHUB_TOKEN")
+    console.error("- $GH_GH_PAT")
+    console.error("- The -t/--token command line argument")
+    throw new TerminationError();
+  }
+
+  const repos: string[] = flags.repo;
+  if (repos.length === 0 && process.env.GITHUB_REPOSITORY) {
+    repos.push(process.env.GITHUB_REPOSITORY);
+  }
+
+  const graphql = new GraphQL("https://api.github.com/graphql", token);
+
+  const locator = new PullRequestLocator(graphql);
+  const prs = await locator.inRepositories(repos);
+  console.log(inspect(prs, { depth: undefined, colors: true }));
 }
--- lib/service/pullRequestLocator.ts
+++ lib/service/pullRequestLocator.ts
@@ -1,6 +1,29 @@
+import {CheckRunStatus} from "../checkRunStatus";
+import {ContextStatus} from "../contextStatus";
 import type {GraphQL} from "../graphQL";
 import {PullRequest} from "../pullRequest";
-import { pullRequestSearchQuery, PullRequestSearchQueryResponse } from "./queries/pullRequestSearch";
+import {RequestedReview} from "../requestedReview";
+import {Review} from "../review";
+import {Status} from "../status";
+import {
+  pullRequestSearchQuery,
+  PullRequestSearchResponse,
+} from "./queries/pullRequestSearch";
+import {
+  rollupPaginationQuery,
+  RollupPaginationResponse,
+} from "./queries/rollupPagination";
+import {
+  isCheckRun,
+  isStatusContext,
+  StatusCheckRollupFragment,
+} from "./queries/statusCheckRollupFragment";
+
+interface RollupPageRequest {
+  rollupId: string;
+  rollupCursor: string;
+  pullRequest: PullRequest;
+}
 
 export class PullRequestLocator {
   graphql: GraphQL;
@@ -14,12 +37,20 @@ export class PullRequestLocator {
     for (const nwo of nwos) {
       searchParts.push(`repo:${nwo}`);
     }
-    const pullRequestData = await this.graphql.query<PullRequestSearchQueryResponse>(pullRequestSearchQuery, {
-      search: searchParts.join(" "),
-    });
+    const pullRequestData = await this.graphql.query<PullRequestSearchResponse>(
+      pullRequestSearchQuery,
+      {
+        search: searchParts.join(" "),
+        rollupCursor: null,
+      }
+    );
 
-    return pullRequestData.search.nodes.map(node => {
-      return new PullRequest(
+    const pullRequests: PullRequest[] = [];
+    const rollupPages: Set<RollupPageRequest> = new Set();
+    for (const node of pullRequestData.search.nodes) {
+      const rollup = node.commits.nodes[0].commit.statusCheckRollup;
+
+      const pullRequest = new PullRequest(
         node.id,
         node.baseRepository.owner.login,
         node.baseRepository.name,
@@ -27,7 +58,127 @@ export class PullRequestLocator {
         node.title,
         node.url,
         node.isDraft,
-      )
-    })
+        this.statuses(rollup),
+        this.requestedReviews(node)
+      );
+
+      const rollupPageInfo = rollup?.contexts.pageInfo;
+      if (rollup && rollupPageInfo?.hasNextPage) {
+        rollupPages.add({
+          rollupId: rollup.id,
+          rollupCursor: rollupPageInfo.endCursor,
+          pullRequest,
+        });
+      }
+
+      pullRequests.push(pullRequest);
+    }
+
+    await this.collectRollupPages(rollupPages);
+
+    return pullRequests;
+  }
+
+  private requestedReviews(
+    pullRequestNode: PullRequestSearchResponse["search"]["nodes"][number]
+  ): RequestedReview[] {
+    const requestedReviewsById = new Map<string, RequestedReview>();
+    for (const reviewRequestNode of pullRequestNode.reviewRequests.nodes) {
+      const teamName = reviewRequestNode.requestedReviewer.slug;
+      if (reviewRequestNode.asCodeOwner && teamName) {
+        requestedReviewsById.set(teamName, new RequestedReview(teamName));
+      }
+    }
+
+    for (const reviewNode of pullRequestNode.reviews.nodes) {
+      const review = new Review(reviewNode.author.login, reviewNode.state);
+
+      for (const onBehalfOfNode of reviewNode.onBehalfOf.nodes) {
+        const teamName = onBehalfOfNode.slug;
+        let requestedReview = requestedReviewsById.get(teamName);
+        if (!requestedReview) {
+          requestedReview = new RequestedReview(teamName);
+          requestedReviewsById.set(teamName, requestedReview);
+        }
+        requestedReview.receivedReviews.push(review);
+      }
+    }
+
+    return Array.from(requestedReviewsById.values());
+  }
+
+  private statuses(rollupNode?: StatusCheckRollupFragment): Status[] {
+    if (!rollupNode) {
+      return [];
+    }
+
+    const statuses: Status[] = [];
+    for (const contextNode of rollupNode.contexts.nodes) {
+      if (isCheckRun(contextNode)) {
+        statuses.push(
+          new CheckRunStatus(
+            contextNode.detailsUrl,
+            contextNode.checkSuite.app.name,
+            contextNode.name,
+            contextNode.status,
+            contextNode.conclusion
+          )
+        );
+      } else if (isStatusContext(contextNode)) {
+        statuses.push(
+          new ContextStatus(
+            contextNode.targetUrl,
+            contextNode.context,
+            contextNode.state
+          )
+        );
+      }
+    }
+    return statuses;
+  }
+
+  private async collectRollupPages(pending: Set<RollupPageRequest>) {
+    let nextRollups = pending;
+    while (nextRollups.size > 0) {
+      nextRollups.clear();
+      await Promise.all(
+        Array.from(pending, async (rollup) => {
+          const nextRollup = await this.getNextRollupPage(rollup);
+          if (nextRollup) {
+            nextRollups.add(nextRollup);
+          }
+        })
+      );
+    }
+  }
+
+  private async getNextRollupPage(
+    rollup: RollupPageRequest
+  ): Promise<RollupPageRequest | null> {
+    const nextRollupPage = await this.graphql.query<RollupPaginationResponse>(
+      rollupPaginationQuery,
+      {
+        rollupId: rollup.rollupId,
+        rollupCursor: rollup.rollupCursor,
+      }
+    );
+
+    const nextRollup = nextRollupPage.node;
+    if (!nextRollup) {
+      return null;
+    }
+
+    rollup.pullRequest.statuses.push(...this.statuses(nextRollupPage.node));
+
+    const pageInfo = nextRollup.contexts.pageInfo;
+    if (pageInfo.hasNextPage) {
+      return {
+        rollupId: nextRollup.id,
+        rollupCursor: pageInfo.endCursor,
+        pullRequest: rollup.pullRequest,
+      };
+    } else {
+      return null;
+    }
   }
 }
