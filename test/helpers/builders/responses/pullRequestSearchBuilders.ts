b84db6286069ada6e8c6ece363fe13e543cbfc35
Ash Wilson <smashwilson@github.com>
2021-12-22 11:10:47 -0500

    GraphQL response builders

---
+++ test/helpers/builders/responses/pullRequestSearchBuilders.ts
@@ -0,0 +1,100 @@
+import {createBuilderClass} from "nested-builder";
+import {PullRequestSearchResponse} from "../../../../lib/service/queries/pullRequestSearch";
+import {StatusCheckRollupFragmentBuilder} from "./statusCheckRollupFragmentBuilders";
+
+type PullRequestNode = PullRequestSearchResponse["search"]["nodes"][number];
+type BaseRepositoryNode = PullRequestNode["baseRepository"];
+type ReviewNode = PullRequestNode["reviews"]["nodes"][number];
+type ReviewRequestNode = PullRequestNode["reviewRequests"]["nodes"][number];
+type CommitNode = PullRequestNode["commits"]["nodes"][number];
+
+const CommitNodeBuilder = createBuilderClass<CommitNode>()({
+  commit: {
+    nested: createBuilderClass<CommitNode["commit"]>()({
+      statusCheckRollup: {
+        nested: StatusCheckRollupFragmentBuilder,
+      },
+    }),
+  },
+});
+
+export const PullRequestSearchBuilder =
+  createBuilderClass<PullRequestSearchResponse>()({
+    search: {
+      nested: createBuilderClass<PullRequestSearchResponse["search"]>()({
+        nodes: {
+          plural: true,
+          nested: createBuilderClass<PullRequestNode>()({
+            id: {default: "SRC000"},
+            isDraft: {default: false},
+            number: {default: 1},
+            title: {default: "Pull Request Title"},
+            url: {default: "https://github.com/owner/repo/pulls/1"},
+            baseRepository: {
+              nested: createBuilderClass<BaseRepositoryNode>()({
+                name: {default: "repo"},
+                owner: {
+                  nested: createBuilderClass<BaseRepositoryNode["owner"]>()({
+                    login: {default: "owner"},
+                  }),
+                },
+              }),
+            },
+            reviews: {
+              nested: createBuilderClass<PullRequestNode["reviews"]>()({
+                nodes: {
+                  plural: true,
+                  nested: createBuilderClass<ReviewNode>()({
+                    author: {
+                      nested: createBuilderClass<ReviewNode["author"]>()({
+                        login: {default: "reviewer"},
+                      }),
+                    },
+                    state: {default: "COMMENTED"},
+                    onBehalfOf: {
+                      nested: createBuilderClass<ReviewNode["onBehalfOf"]>()({
+                        nodes: {
+                          plural: true,
+                          nested: createBuilderClass<
+                            ReviewNode["onBehalfOf"]["nodes"][number]
+                          >()({
+                            slug: {default: "requested-team-slug"},
+                          }),
+                        },
+                      }),
+                    },
+                  }),
+                },
+              }),
+            },
+            reviewRequests: {
+              nested: createBuilderClass<PullRequestNode["reviewRequests"]>()({
+                nodes: {
+                  plural: true,
+                  nested: createBuilderClass<ReviewRequestNode>()({
+                    asCodeOwner: {default: true},
+                    requestedReviewer: {
+                      nested: createBuilderClass<
+                        ReviewRequestNode["requestedReviewer"]
+                      >()({
+                        slug: {default: "requested-team-slug"},
+                      }),
+                    },
+                  }),
+                },
+              }),
+            },
+            commits: {
+              nested: createBuilderClass<PullRequestNode["commits"]>()({
+                nodes: {
+                  plural: true,
+                  nested: CommitNodeBuilder,
+                  default: [new CommitNodeBuilder().build()],
+                },
+              }),
+            },
+          }),
+        },
+      }),
+    },
+  });
---
+++ test/helpers/builders/responses/statusCheckRollupFragmentBuilders.ts
@@ -0,0 +1,54 @@
+import {createBuilderClass} from "nested-builder";
+import {
+  CheckRunResponse,
+  StatusCheckRollupFragment,
+  StatusContextResponse,
+} from "../../../../lib/service/queries/statusCheckRollupFragment";
+
+export const CheckRunBuilder = createBuilderClass<CheckRunResponse>()({
+  __typename: {default: "CheckRun"},
+  id: {default: "ST000"},
+  checkSuite: {
+    nested: createBuilderClass<CheckRunResponse["checkSuite"]>()({
+      app: {
+        nested: createBuilderClass<CheckRunResponse["checkSuite"]["app"]>()({
+          name: {default: "check-suite-app-name"},
+        }),
+      },
+    }),
+  },
+  name: {default: "check-run-name"},
+  title: {default: "check-run-title"},
+  status: {default: "COMPLETED"},
+  conclusion: {default: "SUCCESS"},
+  detailsUrl: {default: "https://example.com"},
+});
+
+export const StatusContextBuilder = createBuilderClass<StatusContextResponse>()(
+  {
+    __typename: {default: "StatusContext"},
+    id: {default: "ST001"},
+    context: {default: "context"},
+    state: {default: "SUCCESS"},
+    targetUrl: {default: "https://example.com"},
+  }
+);
+
+export const StatusCheckRollupFragmentBuilder = createBuilderClass<
+  StatusCheckRollupFragment | undefined
+>()({
+  id: {default: "ST001"},
+  contexts: {
+    nested: createBuilderClass<StatusCheckRollupFragment["contexts"]>()({
+      pageInfo: {
+        nested: createBuilderClass<
+          StatusCheckRollupFragment["contexts"]["pageInfo"]
+        >()({
+          endCursor: {default: "CUR000"},
+          hasNextPage: {default: false},
+        }),
+      },
+      nodes: {plural: true, nested: CheckRunBuilder},
+    }),
+  },
+});
