2e5bb6393541763b7886da8e32bdba8e0c685aa8
Ash Wilson <smashwilson@github.com>
2024-02-09 09:47:28 -0500

    Extract PullRequestFragment from search

--- lib/service/queries/pullRequestSearch.ts
+++ lib/service/queries/pullRequestSearch.ts
@@ -1,111 +1,22 @@
-import {ReviewState} from "../../model/review.js";
 import {
-  StatusCheckRollupFragment,
-  statusCheckRollupFragment,
-} from "./statusCheckRollupFragment.js";
+  PullRequestFragment,
+  pullRequestFragment,
+} from "./pullRequestFragment.js"
 
 export const pullRequestSearchQuery = `
 query($search: String!, $rollupCursor: String) {
   search(query: $search, first: 30, type: ISSUE) {
     nodes {
-      ... on PullRequest {
-        id
-        isDraft
-        number
-        title
-        url
-        baseRepository {
-          name
-          owner {
-            login
-          }
-        }
-
-        # Reviews
-
-        reviews(first: 20) {
-          nodes {
-            author {
-              login
-            }
-            state
-            onBehalfOf(first: 5) {
-              nodes {
-                slug
-              }
-            }
-          }
-        }
-        reviewRequests(first: 20) {
-          nodes {
-            requestedReviewer {
-              ... on Team {
-                slug
-              }
-            }
-          }
-        }
-
-        # Build statuses
-        commits(last: 1) {
-          nodes {
-            commit {
-              statusCheckRollup {
-                ...statusCheckRollup
-              }
-            }
-          }
-        }
-
-      }
+      ...pullRequestFragment
     }
   }
 }
 
-${statusCheckRollupFragment}
+${pullRequestFragment}
 `;
 
 export interface PullRequestSearchResponse {
   search: {
-    nodes: {
-      id: string;
-      isDraft: boolean;
-      number: number;
-      title: string;
-      url: string;
-      baseRepository: {
-        name: string;
-        owner: {
-          login: string;
-        };
-      };
-      reviews: {
-        nodes: {
-          author: {
-            login: string;
-          };
-          state: ReviewState;
-          onBehalfOf: {
-            nodes: {
-              slug: string;
-            }[];
-          };
-        }[];
-      };
-      reviewRequests: {
-        nodes: {
-          requestedReviewer: {
-            slug?: string;
-          };
-        }[];
-      };
-      commits: {
-        nodes: {
-          commit: {
-            statusCheckRollup?: StatusCheckRollupFragment;
-          };
-        }[];
-      };
-    }[];
+    nodes: PullRequestFragment[],
   };
 }
